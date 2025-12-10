af518007a8751ed4797afcf2bac1616df8e071db
Jeyn-o <jenova.fode@gmail.com>
2025-12-10 16:14:25 +0100

    Fix syntax error in test.js

--- scripts/test.js
+++ scripts/test.js
@@ -2,7 +2,7 @@ import { handleEliminations } from "./onElimination.js";
 
 async function test() {
   const fakeTeams = [
-    { id: 9999, name: "Test Team" }
+    { id: 9998, name: "Test Team2" }
   ];
 
   await handleEliminations(fakeTeams);
