39cec05ae6b03b4734d936c47041be1164a45471
Jeyn-o <jenova.fode@gmail.com>
2025-12-10 16:42:04 +0100

    Update checkEliminations.js

--- scripts/checkEliminations.js
+++ scripts/checkEliminations.js
@@ -37,6 +37,6 @@ async function main() {
 console.log("Checking elimination status");
 main();
 
-import { handleEliminations } from "./onElimination.js";
-
-await handleEliminations([{ id: 9997, name: "Test Team3" }]);
+//TEST
+/*import { handleEliminations } from "./onElimination.js";
+await handleEliminations([{ id: 99, name: "Test Team" }]);*/
