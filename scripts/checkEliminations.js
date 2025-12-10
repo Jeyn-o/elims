111f6c4c5b8d966e91f4dd3edac4c413f83380d6
Jeyn-o <jenova.fode@gmail.com>
2025-12-10 15:22:13 +0100

    Remove elimination handling from checkEliminations
    Removed import and call to handleEliminations function.

--- scripts/checkEliminations.js
+++ scripts/checkEliminations.js
@@ -35,6 +35,4 @@ async function main() {
 }
 
 console.log("Checking elimination status");
-const { handleEliminations } = await import("./onElimination.js");
-    await handleEliminations(eliminatedTeams);
 main();
